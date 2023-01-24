c5be2ab492ab4e16af7388e22c035b2eea453aed
pchchv <ipchchv@gmail.com>
2023-01-24 15:03:51 +0300

    Implement a rewindReader

--- formats.go
+++ formats.go
@@ -1,5 +1,10 @@
 package compressor
 
+import (
+	"bytes"
+	"io"
+)
+
 // MatchResult returns true if the format was found either by name, by stream, or by both parameters.
 // The name usually refers to searching by file extension,
 // and the stream refers to reading the first few bytes of the stream (its header).
@@ -10,7 +15,24 @@ type MatchResult struct {
 	ByStream bool
 }
 
+// rewindReader is a reader that can be rewound (reset) to re-read what has already been read
+// and then continue reading further from the main stream. When rewind is no longer needed,
+// call reader() to get a new reader that first reads the buffered bytes and then continues reading from the stream.
+// This is useful for "peeking" into the stream for an arbitrary number of bytes.
+type rewindReader struct {
+	io.Reader
+	buf       *bytes.Buffer
+	bufReader io.Reader
+}
+
 // Matched returns true if a match was made by either name or stream.
 func (mr MatchResult) Matched() bool {
 	return mr.ByName || mr.ByStream
+}
+
+func newRewindReader(r io.Reader) *rewindReader {
+	return &rewindReader{
+		Reader: r,
+		buf:    new(bytes.Buffer),
+	}
 }
