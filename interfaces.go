1fb9818fe959ba89413215ba9ab51f50b28bbeee
pchchv <ipchchv@gmail.com>
2023-01-24 14:41:55 +0300

    Implement a Compression interfaces

--- interfaces.go
+++ interfaces.go
@@ -18,3 +18,24 @@ type Format interface {
 	// you must either buffer what Match reads or search for the last position before calling Match.
 	Match(filename string, stream io.Reader) (MatchResult, error)
 }
+
+// Compressor can compress data by wrapping a writer.
+type Compressor interface {
+	// OpenWriter wraps w with a new writer that compresses what is written.
+	// The writer must be closed when writing is finished.
+	OpenWriter(w io.Writer) (io.WriteCloser, error)
+}
+
+// Decompressor can decompress data by wrapping a reader.
+type Decompressor interface {
+	// OpenReader wraps r with a new reader that decompresses what is read.
+	// The reader must be closed when reading is finished.
+	OpenReader(r io.Reader) (io.ReadCloser, error)
+}
+
+// Compression is a compression format with both compress and decompress methods.
+type Compression interface {
+	Format
+	Compressor
+	Decompressor
+}
