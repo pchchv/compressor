19dd242f3bcff89e541c39be31662007fd8af059
pchchv <ipchchv@gmail.com>
2023-01-28 11:54:06 +0300

    Implement a New & Match methods

--- rar.go
+++ rar.go
@@ -1,5 +1,11 @@
 package compressor
 
+import (
+	"bytes"
+	"io"
+	"strings"
+)
+
 type Rar struct {
 	// If true, errors that occurred while reading or writing a file in the archive
 	// will be logged and the operation will continue for the remaining files.
@@ -9,6 +15,37 @@ type Rar struct {
 	Password string
 }
 
+var (
+	rarHeaderV1_5 = []byte("Rar!\x1a\x07\x00")     // v1.5
+	rarHeaderV5_0 = []byte("Rar!\x1a\x07\x01\x00") // v5.0
+)
+
 func init() {
 	RegisterFormat(Rar{})
+}
+
+func (Rar) Name() string { return ".rar" }
+
+func (r Rar) Match(filename string, stream io.Reader) (MatchResult, error) {
+	var mr MatchResult
+
+	// match filename
+	if strings.Contains(strings.ToLower(filename), r.Name()) {
+		mr.ByName = true
+	}
+
+	// match file header (there are two versions; allocate buffer for larger one)
+	buf, err := readAtMost(stream, len(rarHeaderV5_0))
+	if err != nil {
+		return mr, err
+	}
+
+	matchedV1_5 := len(buf) >= len(rarHeaderV1_5) &&
+		bytes.Equal(rarHeaderV1_5, buf[:len(rarHeaderV1_5)])
+	matchedV5_0 := len(buf) >= len(rarHeaderV5_0) &&
+		bytes.Equal(rarHeaderV5_0, buf[:len(rarHeaderV5_0)])
+
+	mr.ByStream = matchedV1_5 || matchedV5_0
+
+	return mr, nil
 }
