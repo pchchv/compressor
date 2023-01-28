8166ffeb22566184828178e749cdd14a6dd93067
pchchv <ipchchv@gmail.com>
2023-01-28 12:21:34 +0300

    Implement a Name & Archive methods

--- 7zip.go
+++ 7zip.go
@@ -1,5 +1,11 @@
 package compressor
 
+import (
+	"context"
+	"fmt"
+	"io"
+)
+
 type SevenZip struct {
 	// If true, errors that occurred while reading or writing a file in the archive
 	// will be logged and the operation will continue for the remaining files.
@@ -11,4 +17,14 @@ type SevenZip struct {
 
 func init() {
 	RegisterFormat(SevenZip{})
+}
+
+func (z SevenZip) Name() string {
+	return ".7z"
+}
+
+// Archive is not implemented for 7z,
+// but the method exists so that SevenZip satisfies the ArchiveFormat interface.
+func (z SevenZip) Archive(_ context.Context, _ io.Writer, _ []File) error {
+	return fmt.Errorf("not implemented for 7z because there is no pure Go implementation found")
 }
