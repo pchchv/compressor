1873a9cea2382e2e48d51974003652567ef4c4f2
pchchv <ipchchv@gmail.com>
2023-01-28 14:04:40 +0300

    Implement a Match method

--- bz2.go
+++ bz2.go
@@ -1,9 +1,17 @@
 package compressor
 
+import (
+	"bytes"
+	"io"
+	"strings"
+)
+
 // Bz2 facilitates bzip2 compression.
 type Bz2 struct {
 	CompressionLevel int
 }
+
+var bzip2Header = []byte("BZh")
 
 func init() {
 	RegisterFormat(Bz2{})
@@ -11,4 +19,23 @@ func init() {
 
 func (Bz2) Name() string {
 	return ".bz2"
+}
+
+func (bz Bz2) Match(filename string, stream io.Reader) (MatchResult, error) {
+	var mr MatchResult
+
+	// match filename
+	if strings.Contains(strings.ToLower(filename), bz.Name()) {
+		mr.ByName = true
+	}
+
+	// match file header
+	buf, err := readAtMost(stream, len(bzip2Header))
+	if err != nil {
+		return mr, err
+	}
+
+	mr.ByStream = bytes.Equal(buf, bzip2Header)
+
+	return mr, nil
 }
