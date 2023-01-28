dd523c726d03eb23a281ead2e43c16c83a6f09f8
pchchv <ipchchv@gmail.com>
2023-01-28 12:23:14 +0300

    Implement a Match method

--- 7zip.go
+++ 7zip.go
@@ -1,9 +1,11 @@
 package compressor
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"io"
+	"strings"
 )
 
 type SevenZip struct {
@@ -15,12 +17,33 @@ type SevenZip struct {
 	Password string
 }
 
+var sevenZipHeader = []byte("7z\xBC\xAF\x27\x1C")
+
 func init() {
 	RegisterFormat(SevenZip{})
 }
 
 func (z SevenZip) Name() string {
 	return ".7z"
+}
+
+func (z SevenZip) Match(filename string, stream io.Reader) (MatchResult, error) {
+	var mr MatchResult
+
+	// match filename
+	if strings.Contains(strings.ToLower(filename), z.Name()) {
+		mr.ByName = true
+	}
+
+	// match file header
+	buf, err := readAtMost(stream, len(sevenZipHeader))
+	if err != nil {
+		return mr, err
+	}
+
+	mr.ByStream = bytes.Equal(buf, sevenZipHeader)
+
+	return mr, nil
 }
 
 // Archive is not implemented for 7z,
