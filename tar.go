61fecd200294418b2d620937d6ea077e31f4727e
pchchv <ipchchv@gmail.com>
2023-01-28 12:56:58 +0300

    Implement a Name & Match methods

--- tar.go
+++ tar.go
@@ -1,5 +1,11 @@
 package compressor
 
+import (
+	"archive/tar"
+	"io"
+	"strings"
+)
+
 type Tar struct {
 	// If true, errors that occurred while reading or writing a file in the archive
 	// will be logged and the operation will continue for the remaining files.
@@ -8,4 +14,24 @@ type Tar struct {
 
 func init() {
 	RegisterFormat(Tar{})
+}
+
+func (Tar) Name() string {
+	return ".tar"
+}
+
+func (t Tar) Match(filename string, stream io.Reader) (MatchResult, error) {
+	var mr MatchResult
+
+	// match filename
+	if strings.Contains(strings.ToLower(filename), t.Name()) {
+		mr.ByName = true
+	}
+
+	// match file header
+	r := tar.NewReader(stream)
+	_, err := r.Next()
+	mr.ByStream = err == nil
+
+	return mr, nil
 }
