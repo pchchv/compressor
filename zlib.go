932452757fa3b7270da9f1034563de5c092ab97a
pchchv <ipchchv@gmail.com>
2023-01-29 08:38:25 +0300

    Implement a Name & Match methods

--- zlib.go
+++ zlib.go
@@ -1,10 +1,41 @@
 package compressor
 
+import (
+	"bytes"
+	"io"
+	"strings"
+)
+
 // Zlib facilitates zlib compression.
 type Zlib struct {
 	CompressionLevel int
 }
 
+var ZlibHeader = []byte{0x78}
+
 func init() {
 	RegisterFormat(Zlib{})
+}
+
+func (Zlib) Name() string {
+	return ".zz"
+}
+
+func (zz Zlib) Match(filename string, stream io.Reader) (MatchResult, error) {
+	var mr MatchResult
+
+	// match filename
+	if strings.Contains(strings.ToLower(filename), zz.Name()) {
+		mr.ByName = true
+	}
+
+	// match file header
+	buf, err := readAtMost(stream, len(ZlibHeader))
+	if err != nil {
+		return mr, err
+	}
+
+	mr.ByStream = bytes.Equal(buf, ZlibHeader)
+
+	return mr, nil
 }
