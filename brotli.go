091340080df226810944f69b0f6837524021e17e
pchchv <ipchchv@gmail.com>
2023-01-29 08:33:50 +0300

    Implement a Name & Match methods

--- brotli.go
+++ brotli.go
@@ -1,5 +1,10 @@
 package compressor
 
+import (
+	"io"
+	"strings"
+)
+
 // Brotli facilitates brotli compression.
 type Brotli struct {
 	Quality int
@@ -7,4 +12,23 @@ type Brotli struct {
 
 func init() {
 	RegisterFormat(Brotli{})
+}
+
+func (Brotli) Name() string {
+	return ".br"
+}
+
+func (br Brotli) Match(filename string, stream io.Reader) (MatchResult, error) {
+	var mr MatchResult
+
+	// match filename
+	if strings.Contains(strings.ToLower(filename), br.Name()) {
+		mr.ByName = true
+	}
+
+	// brotli does not have well-defined file headers.
+	// The best way to match a stream would be to try to decode part of it,
+	// and that has not yet been implemented
+
+	return mr, nil
 }
