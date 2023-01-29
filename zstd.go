2c32f913b82a5ab90526fc2254b29fe422a0330f
pchchv <ipchchv@gmail.com>
2023-01-29 08:49:35 +0300

    Implement a Name & Match methods

--- zstd.go
+++ zstd.go
@@ -1,6 +1,12 @@
 package compressor
 
-import "github.com/klauspost/compress/zstd"
+import (
+	"bytes"
+	"io"
+	"strings"
+
+	"github.com/klauspost/compress/zstd"
+)
 
 // Zstd facilitates Zstandard compression.
 type Zstd struct {
@@ -8,6 +14,32 @@ type Zstd struct {
 	DecoderOptions []zstd.DOption
 }
 
+// magic number at the beginning of Zstandard files
+var zstdHeader = []byte{0x28, 0xb5, 0x2f, 0xfd}
+
 func init() {
 	RegisterFormat(Zstd{})
+}
+
+func (Zstd) Name() string {
+	return ".zst"
+}
+
+func (zs Zstd) Match(filename string, stream io.Reader) (MatchResult, error) {
+	var mr MatchResult
+
+	// match filename
+	if strings.Contains(strings.ToLower(filename), zs.Name()) {
+		mr.ByName = true
+	}
+
+	// match file header
+	buf, err := readAtMost(stream, len(zstdHeader))
+	if err != nil {
+		return mr, err
+	}
+
+	mr.ByStream = bytes.Equal(buf, zstdHeader)
+
+	return mr, nil
 }
