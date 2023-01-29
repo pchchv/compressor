29acc7cbf992f8dd835672982f457f9a43ce900b
pchchv <ipchchv@gmail.com>
2023-01-29 08:54:45 +0300

    Implement a Name & Match methods

--- sz.go
+++ sz.go
@@ -1,8 +1,39 @@
 package compressor
 
+import (
+	"bytes"
+	"io"
+	"strings"
+)
+
 // Sz facilitates Snappy compression.
 type Sz struct{}
 
+var snappyHeader = []byte{0xff, 0x06, 0x00, 0x00, 0x73, 0x4e, 0x61, 0x50, 0x70, 0x59}
+
 func init() {
 	RegisterFormat(Sz{})
+}
+
+func (sz Sz) Name() string {
+	return ".sz"
+}
+
+func (sz Sz) Match(filename string, stream io.Reader) (MatchResult, error) {
+	var mr MatchResult
+
+	// match filename
+	if strings.Contains(strings.ToLower(filename), sz.Name()) {
+		mr.ByName = true
+	}
+
+	// match file header
+	buf, err := readAtMost(stream, len(snappyHeader))
+	if err != nil {
+		return mr, err
+	}
+
+	mr.ByStream = bytes.Equal(buf, snappyHeader)
+
+	return mr, nil
 }
