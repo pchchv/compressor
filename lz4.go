e6ba2d80443c363ed02c30877941c159d5d259c6
pchchv <ipchchv@gmail.com>
2023-01-29 08:41:02 +0300

    Implement a Name & Match methods

--- lz4.go
+++ lz4.go
@@ -1,10 +1,41 @@
 package compressor
 
+import (
+	"bytes"
+	"io"
+	"strings"
+)
+
 // Lz4 facilitates LZ4 compression.
 type Lz4 struct {
 	CompressionLevel int
 }
 
+var lz4Header = []byte{0x04, 0x22, 0x4d, 0x18}
+
 func init() {
 	RegisterFormat(Lz4{})
+}
+
+func (Lz4) Name() string {
+	return ".lz4"
+}
+
+func (lz Lz4) Match(filename string, stream io.Reader) (MatchResult, error) {
+	var mr MatchResult
+
+	// match filename
+	if strings.Contains(strings.ToLower(filename), lz.Name()) {
+		mr.ByName = true
+	}
+
+	// match file header
+	buf, err := readAtMost(stream, len(lz4Header))
+	if err != nil {
+		return mr, err
+	}
+
+	mr.ByStream = bytes.Equal(buf, lz4Header)
+
+	return mr, nil
 }
